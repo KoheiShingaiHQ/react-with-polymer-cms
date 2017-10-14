49a39e54e9ad6569e16eec21d490536a09ffb823
koheishingai <koheishingai+HQ@gmail.com>
2017-10-14 23:04:39 +0900

    Featured page with React data binding : React + Firebase + GitHub pages like CMS

--- src/container/ContentsFull.js
+++ src/container/ContentsFull.js
@@ -2,30 +2,32 @@ import React, { Component } from 'react';
 import Content from '../container/Content.js';
 
 class ContentsFull extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { contents : [] }
+  }
+  componentDidMount() {
+    var contents = [];
+    var data = this.props.data;
+    for(var i in data){
+      contents.push(
+        <li key={data[i].main} style={{backgroundImage:"url("+ data[i].image +")", height:data[i].height}}>
+          <div>
+            <Content full_main={data[i].main} full_sub={data[i].sub} color={data[i].color}></Content>
+            <div></div>
+          </div>
+        </li>  
+      );
+    }
+    this.setState({ contents : contents });
+  }
   render() {
     return (
       <section id="contents-full">
-        <ul>{contents}</ul>
+        <ul>{this.state.contents}</ul>
       </section>
     );
   }
-}
-
-var contents = [];
-var data = [
-  {main: "Article 1", sub: 'Something about this article 1'},
-  {main: "Article 2", sub: 'Something about this article 2'},
-  {main: "Article 3", sub: 'Something about this article 3'},
-];
-for(var i in data){
-  contents.push(
-    <li key={data[i].main}>
-      <div>
-        <Content full_main={data[i].main} full_sub={data[i].sub}></Content>
-        <div></div>
-      </div>
-    </li>  
-  );
 }
 
 export default ContentsFull;
--- src/container/Featured.js
+++ src/container/Featured.js
@@ -8,10 +8,21 @@ class Featured extends Component {
     super(props);
   }
   render() {
+    const fullContentData = [
+      {main: "Yellowstone", sub: "The Grand Canyon of the Yellowstone", color: "rgba(255, 255, 255, .88)", height: "360px", image: "https://www.nationalgeographic.com/content/dam/photography/rights-exempt/Best-of-2016/grandcanyon.adapt.1900.1.jpg"},
+      {main: "Acadia", sub: 'An autumn-hued forest with morning fog', color: "rgba(255, 255, 255, .98)", height: "360px", image: "https://www.nationalgeographic.com/content/dam/photography/PROOF/2017/September/fall-equinox/01-fall-gallery-NationalGeographic_977935.ngsversion.1505910666082.adapt.1190.1.jpg"},
+      {main: "Torres del Paine", sub: 'Walking with my wife, Allie', color: "rgba(0, 0, 0, .78)", height: "600px", image: "https://www.nationalgeographic.com/content/dam/photography/PROOF/2017/May/travel-photo-contest-gallery/36-10201526_uploadsmember1502011yourshot-1502011-10201526jpg_c76qs3taxrcmefapc6rxxj4gjhp3eflutfvvbpyjwjhzlmh4iziq_5616x3744.ngsversion.1494945573599.adapt.1190.1.jpg"}
+    ];
+    const halfContentData = [
+      {main: "King Penguin", sub: 'An excellent diver', color: "rgba(255, 255, 255, .98)", image: "https://news.nationalgeographic.com/content/dam/news/2017/04/25/penguin-gallery/04-penguin-photo-ark.ngsversion.1492806839404.adapt.1190.1.jpg"},
+      {main: "Border Collies", sub: 'Take that, Lassie', color: "rgba(255, 255, 255, .98)", image: "https://news.nationalgeographic.com/content/dam/news/2016/10/04/animal_day_gallery/06_animal_day_gallery_border_collie.ngsversion.1475593210354.adapt.1190.1.jpg"},
+      {main: "Bornean Orangutan", sub: 'Grand prize winner', color: "rgba(255, 255, 255, .98)", image: "https://news.nationalgeographic.com/content/dam/news/2016/10/17/wpy-winners/2016-wpy-winners-07.ngsversion.1476829830918.adapt.945.1.jpg"},
+      {main: "African Elephant", sub: 'Young elephants often play', color: "rgba(255, 255, 255, .98)", image: "https://news.nationalgeographic.com/content/dam/news/2016/08/12/elephant/10_elephant_day_gallery.ngsversion.1502461851681.adapt.1190.1.jpg"}
+    ];
     return (
       <main id="featured">
-        <ContentsFull></ContentsFull>
-        <ContentsHalf></ContentsHalf>
+        <ContentsFull data={fullContentData}></ContentsFull>
+        <ContentsHalf data={halfContentData}></ContentsHalf>
       </main>
     );
   }
